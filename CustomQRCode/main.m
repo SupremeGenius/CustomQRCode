ebcb921b543583b7f5b83a01616a07ed86905638
chengxun <chengxun08@163.com>
2015-12-30 09:59:00 +0800

    Initial Commit

---
+++ CustomQRCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomQRCode
+//
+//  Created by chengxun on 15/12/30.
+//  Copyright © 2015年 chengxun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
